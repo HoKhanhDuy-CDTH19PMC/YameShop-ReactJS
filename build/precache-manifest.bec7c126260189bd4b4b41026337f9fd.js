5a6448491d6046f552413c4cfed352387f517a58
KhanhDuy2403 <67962641+KhanhDuy2403@users.noreply.github.com>
2022-03-11 12:10:09 +0700

    yameshop with react

---
+++ build/precache-manifest.bec7c126260189bd4b4b41026337f9fd.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb17b6827ed8d2727f5479db2b295b3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75afc3395dc16d138733",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "c502b3d951d2b245043e",
+    "url": "/static/css/main.334d997d.chunk.css"
+  },
+  {
+    "revision": "75afc3395dc16d138733",
+    "url": "/static/js/2.e545bbdb.chunk.js"
+  },
+  {
+    "revision": "beec76944f44b9d7fa8580bee39033f4",
+    "url": "/static/js/2.e545bbdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ee69ee430cd255495c9",
+    "url": "/static/js/3.f4b96d33.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/3.f4b96d33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "290e14e1df9bcaa058c8",
+    "url": "/static/js/4.3f4361c3.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/static/js/4.3f4361c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a1fb6a82eb423d31e30",
+    "url": "/static/js/5.484c6d41.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.484c6d41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c502b3d951d2b245043e",
+    "url": "/static/js/main.ff5a6db6.chunk.js"
+  },
+  {
+    "revision": "54d6d36ad14920aead07",
+    "url": "/static/js/runtime-main.ea334993.js"
+  },
+  {
+    "revision": "63412a1c3ac3b720b9ded6f8a1b60427",
+    "url": "/static/media/Left.63412a1c.png"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  },
+  {
+    "revision": "e39cd8545a34fce25998696c88b400ea",
+    "url": "/static/media/Right.e39cd854.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
